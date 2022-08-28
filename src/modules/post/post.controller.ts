6f12bc37599e2911e4985fcd68f28ba4da590083
Samuel Seyi <adebayosamueljahsmine30@gmail.com>
2022-08-28 10:47:39 +0100

    added get update and delete posts endpoints

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { AuthUser } from '../user/decorator';
 import { IUser } from '../user/interface';
-import { CreatePostDto } from './dto';
+import { CreatePostDto, UpdatePostDto } from './dto';
 import { PostService } from './post.service';
 
 @Controller('posts')
@@ -15,6 +23,39 @@ export class PostController {
     return {
       message: 'Post created successfully',
       data: post,
+    };
+  }
+
+  @Get()
+  async myPosts(@AuthUser() user: IUser) {
+    const posts = await this.service.find({ owner: user.id });
+
+    return {
+      message: 'Posts retrieved successfully',
+      data: posts,
+    };
+  }
+
+  @Put(':id')
+  async update(
+    @Body() payload: UpdatePostDto,
+    @AuthUser() user: IUser,
+    @Param('id') id: number,
+  ) {
+    const post = await this.service.update(id, user, payload);
+
+    return {
+      message: 'Post updated successfully',
+      data: post,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@AuthUser() user: IUser, @Param('id') id: number) {
+    await this.service.delete(id, user);
+
+    return {
+      message: 'Post deleted successfully',
     };
   }
 }
