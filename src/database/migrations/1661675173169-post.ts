5ad0e75e6d6c02a13f5afd593fef147eeaaed411
Samuel Seyi <adebayosamueljahsmine30@gmail.com>
2022-08-28 10:52:11 +0100

    added migration file for post table

--- src/database/migrations/1661675173169-post.ts
+++ src/database/migrations/1661675173169-post.ts
@@ -1,11 +1,31 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class post1661675173169 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'post',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+        ],
+      }),
+      true,
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('post');
+  }
 }
